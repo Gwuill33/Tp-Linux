2e5ad7faf62a2198433902ba2ac8a395a0fe02ed
Gwuill <guillaume.branche@ynov.com>
2022-12-04 23:01:08 +0100

    YT

--- 3/readme.md
+++ 3/readme.md
@@ -1,7 +1,7 @@
 **Partie I**
 ```bash
-[gwuill@localhost ~]$ bash /srv/idcard/idcard.sh
-Machine name : localhost
+[gwuill@tp3-linux ~]$ bash /srv/idcard/idcard.sh
+Machine name : tp3-linux
 OS Rocky Linux release 9.0 (Blue Onyx) and kernel version is Linux 5.14.0-70.26.1.el9_0.x86_64
 IP : 10.0.2.15/24
 RAM : 653M memory available on 960M total memory
---
+++ srv/yt/yt.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ ! -d '/srv/yt/downloads' ]]; then
+    echo "Error: downloads/ does not exist"
+    exit 1
+fi
+
+title="$(youtube-dl --get-filename -o '%(title)s' $1)"
+filename="$(youtube-dl --get-filename -o '%(title)s.%(ext)s' $1)"
+description="$(youtube-dl --get-description $1)"
+
+dest_dir="/srv/yt/downloads/${title}"
+
+if [[ ! -d "${dest_dir}" ]]; then
+    echo "Error $title/ does not exist. Creating."
+    mkdir "${dest_dir}"
+fi
+
+youtube-dl -o "/srv/yt/downloads/${title}/${filename}" "$1"
+touch "/srv/yt/downloads/${title}/description" 
+$(youtube-dl --get-description $1) > "/srv/yt/downloads/${title}/description"
+
+if [[ ! -d '/var/log/yt/' ]]; then
+	echo "Error : download.log does not exist"
+	exit 1
+fi
+
+$(sudo date +"[%y/%m/%d %H:%M:%S]") "Video $1 was downloaded. File path : /srv/yt/downloads/${title}/${filename}" >> "/var/log/yt/download.log"
+echo "Video $1 was downloaded."
+echo "File path /srv/yt/downloads/${title}/${filename}"
